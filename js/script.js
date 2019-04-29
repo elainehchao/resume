3196b9d3d68774cae8edadc4224f9f8ecf45a6d5
e5chao <e5chao@ucsd.edu>
2019-04-28 20:28:14 -0700

    Style the accordion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(function () {
     $("#accordion").accordion({
+        collapsible: true,
+        active: false
+    });
 
-    })
+    $("#accordion").accordion("option", "icons", {
+        'header': 'ui-icon-triangle-1-e',
+        'activeHeader': 'ui-icon-triangle-1-s'
+    });
 });
